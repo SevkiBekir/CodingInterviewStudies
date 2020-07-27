d99fbd9dc8b5041c6c2232649af86906dbf3f703
Sevki Kocadag <sbk@Sevkis-MacBook-Pro.local>
2020-07-27 21:59:13 +0300

    added q1_6 in python

---
+++ CrackingTheCode/Chapter1/question_1_6.py
@@ -0,0 +1,42 @@
+def compress(text):
+    result = text[0] + str(len(text))
+    return result
+
+
+def compressString(text):
+    resultText = ""
+
+    countedText = ""
+    for char in text:
+        # # check last character
+        # if charIndex == textLength - 1:
+        #     continue
+
+
+        if countedText == "" or char in countedText:
+            # added the char
+            countedText += char
+        else:
+            # not the same
+            # first compress it
+            resultText +=compress(countedText)
+            # added the current char
+            countedText = char
+
+
+    # last char to add
+    resultText += compress(countedText)
+
+    if len(resultText) > len(text):
+        resultText = text
+
+    return resultText
+
+if __name__ == '__main__':
+    text = "aabcccccaaa"
+    result = compressString(text)
+    print(result)
+
+    text = "compressed"
+    result = compressString(text)
+    print(result)
