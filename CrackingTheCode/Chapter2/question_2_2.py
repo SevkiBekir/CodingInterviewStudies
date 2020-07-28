72c6a1fa451b8033e52f945e3c3ec4100005102b
Sevki Kocadag <bekirsevki@gmail.com>
2020-07-28 22:20:23 +0300

    added question 2_2 in python

---
+++ CrackingTheCode/Chapter2/question_2_2.py
@@ -0,0 +1,30 @@
+from Chapter2.LinkedList import LinkedList
+
+
+def findKthElementFromLast(kth,linkedList):
+
+    if kth > linkedList.size or kth < 1:
+        print("Out of scope")
+        return None
+
+    index = linkedList.size - kth
+    kthNode = linkedList.head
+    for i in range(index):
+        if kthNode is not None:
+            kthNode = kthNode.next
+
+    return kthNode.data
+
+
+
+if __name__ == '__main__':
+    l1 = LinkedList()
+    l1.insertTail(1)
+    l1.insertTail(2)
+    l1.insertTail(3)
+    l1.insertTail(4)
+
+    l1.printList()
+    print("findingKthElement")
+    result = findKthElementFromLast(5,l1)
+    print(result)
