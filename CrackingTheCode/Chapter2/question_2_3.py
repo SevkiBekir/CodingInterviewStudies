350a7537ea01d9df99199c8c9acad860508137a8
Sevki Kocadag <bekirsevki@gmail.com>
2020-07-29 13:59:28 +0300

    added question 2_3 in python

---
+++ CrackingTheCode/Chapter2/question_2_3.py
@@ -0,0 +1,15 @@
+from Chapter2.LinkedList import LinkedList
+
+if __name__ == '__main__':
+    l1 = LinkedList()
+    l1.insertTail(1)
+    l1.insertTail(2)
+    l1.insertTail(3)
+    l1.insertTail(4)
+
+    l1.printList()
+
+    l1.removeNode(3)
+    l1.printList()
+
+
